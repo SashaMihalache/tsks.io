f83d6f739a05eae80383f0d9fb494cea5659c82a
sasha mihalache <sasha.mihalache@gmail.com>
2022-11-11 15:09:44 +0200

    add tailwind support cross-project

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -11,6 +11,7 @@ const Home: NextPage = () => (
   <div className="flex min-h-screen flex-col items-center justify-center py-2">
     {coolKid.amICool ? 'I am cool' : 'I am not cool'}
     <Button />
+    <div className=" bg-yellow-200">Next-app</div>
   </div>
 )
 
