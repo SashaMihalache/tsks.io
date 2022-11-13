13627dacc685f6643ab47fefc9985d76a44a5dde
sasha mihalache <sasha.mihalache@gmail.com>
2022-11-13 14:24:35 +0200

    test ci/cd

--- apps/next-app/src/components/HelloWorld.tsx
+++ apps/next-app/src/components/HelloWorld.tsx
@@ -3,7 +3,7 @@ import { Button } from 'ui'
 
 const HelloWorld = () => (
   <>
-    <h1>Helllo</h1>
+    <h1>Helllo there</h1>
     <label htmlFor="name">
       <input id="name" name="name" type="text" />
     </label>
--- apps/server/src/lib/add.test.ts
+++ apps/server/src/lib/add.test.ts
@@ -2,6 +2,6 @@ import add from './add'
 
 describe('add', () => {
   it('should add two numbers', () => {
-    expect(add(1, 2)).toEqual(3)
+    expect(add(1, 3)).toEqual(4)
   })
 })
