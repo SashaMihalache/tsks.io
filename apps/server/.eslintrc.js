63f057e78d724c9abfd79050737aa0aa6ba125cc
sasha mihalache <sasha.mihalache@gmail.com>
2022-11-13 14:08:59 +0200

    fix lint on server

--- apps/server/.eslintrc.js
+++ apps/server/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ...require('config/eslint-server'),
   parserOptions: {
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.lint.json'],
     tsconfigRootDir: __dirname,
   },
 }
