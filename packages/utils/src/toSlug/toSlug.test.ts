31e2367b85b13032eb795fb27e5ef00d8a5540dd
sasha mihalache <sasha.mihalache@gmail.com>
2022-11-16 15:51:57 +0200

    test commit

--- packages/utils/src/toSlug/toSlug.test.ts
+++ packages/utils/src/toSlug/toSlug.test.ts
@@ -1,7 +1,7 @@
 import { toSlug } from './toSlug'
 
 describe('toSlug', () => {
-  it('should convert a string to a slug', () => {
+  it('should convert a string to a slug with dashes', () => {
     expect(toSlug('Hello World')).toEqual('hello-world')
   })
 })
